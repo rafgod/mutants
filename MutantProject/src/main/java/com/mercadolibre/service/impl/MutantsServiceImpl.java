165d7f264add3eb531f27219071d24e3db8e7a49
rafgod <52473323+rafgod@users.noreply.github.com>
2019-08-05 17:06:59 -0300

    reestructuracion de codigo
    reestructuracion de codigo

--- MutantProject/src/main/java/com/mercadolibre/service/impl/MutantsServiceImpl.java
+++ MutantProject/src/main/java/com/mercadolibre/service/impl/MutantsServiceImpl.java
@@ -34,26 +34,34 @@ public class MutantsServiceImpl implements MutantsService {
 	    isMutant = dnaRecord.get().getIsMutant();
 	} else {
 	    isMutant = detectMutantDNA(dna);
-	    DNA newDNARecord = new DNA();
-	    newDNARecord.setDnaSequence(dnaString);
-	    newDNARecord.setIsMutant(isMutant);
-	    dnaRepository.save(newDNARecord);
+	    saveDNA(isMutant, dnaString);
 	}
-
 	return isMutant;
     }
 
-    private static Boolean detectMutantDNA(String[] dnaMutant) {
+    private void saveDNA(Boolean isMutant, String dnaString) {
+	DNA newDNARecord = new DNA();
+	newDNARecord.setDnaSequence(dnaString);
+	newDNARecord.setIsMutant(isMutant);
+	dnaRepository.save(newDNARecord);
+    }
+
+    private Boolean detectMutantDNA(String[] dnaMutant) {
 
 	List<String> wordList = new ArrayList<>();
 	int dnaLen = dnaMutant.length;
+
+	wordList.addAll(checkDiagonalLeftToRight(dnaMutant));
+	if (wordList.size() > 1) {
+	    return true;
+	}
+	wordList.addAll(checkDiagonalRightToLeft(dnaMutant));
+	if (wordList.size() > 1) {
+	    return true;
+	}
+
 	for (int x = 0; x < dnaLen; x++) {
-	    if (wordList.size() > 1) {
-		return true;
-	    }
-	    StringBuilder vertical = new StringBuilder();
-	    StringBuilder diagonal = new StringBuilder();
-	    StringBuilder diagonalLeft = new StringBuilder();
+
 	    if (dnaMutant[x].contains(MutantSequences.A.getSequence())) {
 		wordList.add(MutantSequences.A.getSequence());
 	    } else if (dnaMutant[x].contains(MutantSequences.T.getSequence())) {
@@ -63,48 +71,13 @@ public class MutantsServiceImpl implements MutantsService {
 	    } else if (dnaMutant[x].contains(MutantSequences.C.getSequence())) {
 		wordList.add(MutantSequences.C.getSequence());
 	    }
-	    if (x == 1) {
-		for (int dr = 0; dr < dnaLen && dr + 1 < dnaLen; dr++) {
-		    if (diagonal.toString().isEmpty() || diagonal.charAt(diagonal.length() - 1) == dnaMutant[dr].charAt(dr + 1)) {
-			diagonal.append(dnaMutant[dr].charAt(dr + 1));
-		    } else if (diagonal.length() < 4) {
-			diagonal = new StringBuilder();
-		    }
-		}
-	    } else if (x >= SECUENCES_DNA_LENGTH - 1) {
-		int auxy = 0;
-		for (int dl = x; dl >= 0; dl--) {
-		    if (diagonalLeft.toString().isEmpty() || diagonalLeft.charAt(diagonalLeft.length() - 1) == dnaMutant[auxy].charAt(dl)) {
-			diagonalLeft.append(dnaMutant[auxy].charAt(dl));
-		    } else if (diagonalLeft.length() < SECUENCES_DNA_LENGTH) {
-			diagonalLeft = new StringBuilder();
-		    }
-		    if (dl < SECUENCES_DNA_LENGTH && diagonalLeft.toString().isEmpty()) {
-			break;
-		    }
-		    auxy++;
-		}
-		diagonalLeft = new StringBuilder();
-		if (x == dnaLen) {
-		    auxy = 1;
-		    while (auxy <= dnaLen - SECUENCES_DNA_LENGTH) {
-			for (int dl = x, z = auxy; dl >= 0; dl--, z++) {
-			    if (diagonalLeft.toString().isEmpty() || diagonalLeft.charAt(diagonalLeft.length() - 1) == dnaMutant[z].charAt(dl)) {
-				diagonalLeft.append(dnaMutant[z].charAt(dl));
-			    } else if (diagonalLeft.length() < SECUENCES_DNA_LENGTH) {
-				diagonalLeft = new StringBuilder();
-			    }
-			}
-			auxy++;
-		    }
 
-		}
-		if (diagonalLeft.length() >= SECUENCES_DNA_LENGTH) {
-		    wordList.add(diagonalLeft.toString());
-		}
+	    if (wordList.size() > 1) {
+		return true;
 	    }
 
 	    for (int y = 0; y < dnaLen; y++) {
+		StringBuilder vertical = new StringBuilder();
 		if (vertical.toString().isEmpty() || vertical.charAt(vertical.length() - 1) == dnaMutant[y].charAt(x)) {
 		    vertical.append(dnaMutant[y].charAt(x));
 		} else if (vertical.length() < SECUENCES_DNA_LENGTH) {
@@ -112,32 +85,11 @@ public class MutantsServiceImpl implements MutantsService {
 		} else {
 		    break;
 		}
-
-		diagonal = new StringBuilder();
-		if (x == 0) {
-		    if (diagonal.toString().isEmpty()) {
-			diagonal.append(dnaMutant[y].charAt(x));
-		    }
-		    for (int dr = 1; dr < dnaLen && dr + y < dnaLen; dr++) {
-			if (diagonal.toString().isEmpty() || diagonal.charAt(diagonal.length() - 1) == dnaMutant[dr + y].charAt(dr)) {
-			    diagonal.append(dnaMutant[dr + y].charAt(dr));
-			} else if (diagonal.length() < SECUENCES_DNA_LENGTH) {
-			    diagonal = new StringBuilder();
-			} else {
-			    break;
-			}
-		    }
-		}
-
-		if (diagonal.length() >= SECUENCES_DNA_LENGTH) {
-		    wordList.add(diagonal.toString());
-		}
-
-		if (wordList.size() > 1) {
-		    return true;
-		}
 		if (vertical.length() >= SECUENCES_DNA_LENGTH) {
 		    wordList.add(vertical.toString());
+		    if (wordList.size() > 1) {
+			return true;
+		    }
 		    break;
 		}
 
@@ -147,6 +99,93 @@ public class MutantsServiceImpl implements MutantsService {
 
 	return wordList.size() > 1;
 
+    }
+
+    private List<String> checkDiagonalRightToLeft(String[] dnaMutant) {
+	StringBuilder diagonalLeft = new StringBuilder();
+	int dnaLen = dnaMutant.length;
+	List<String> wordList = new ArrayList<>();
+
+	for (int yr = SECUENCES_DNA_LENGTH - 1; yr < dnaLen; yr++) {
+	    int auxx = 0;
+	    diagonalLeft = new StringBuilder();
+	    for (int dl = yr; dl >= 0; dl--) {
+		if (diagonalLeft.toString().isEmpty() || diagonalLeft.charAt(diagonalLeft.length() - 1) == dnaMutant[auxx].charAt(dl)) {
+		    diagonalLeft.append(dnaMutant[auxx].charAt(dl));
+		} else if (diagonalLeft.length() < SECUENCES_DNA_LENGTH) {
+		    diagonalLeft = new StringBuilder();
+		}
+		if (dl < SECUENCES_DNA_LENGTH && diagonalLeft.toString().isEmpty()) {
+		    break;
+		}
+		auxx++;
+	    }
+	    if (diagonalLeft.length() >= SECUENCES_DNA_LENGTH) {
+		wordList.add(diagonalLeft.toString());
+	    }
+	}
+
+	for (int auxy = 1; auxy <= dnaLen - SECUENCES_DNA_LENGTH; auxy++) {
+	    diagonalLeft = new StringBuilder();
+	    for (int dl = dnaLen - 1, z = auxy; dl >= 0 && z < dnaLen; dl--, z++) {
+		if (diagonalLeft.toString().isEmpty() || diagonalLeft.charAt(diagonalLeft.length() - 1) == dnaMutant[z].charAt(dl)) {
+		    diagonalLeft.append(dnaMutant[z].charAt(dl));
+		} else if (diagonalLeft.length() < SECUENCES_DNA_LENGTH) {
+		    diagonalLeft = new StringBuilder();
+		}
+	    }
+	    if (diagonalLeft.length() >= SECUENCES_DNA_LENGTH) {
+		wordList.add(diagonalLeft.toString());
+	    }
+	}
+
+	return wordList;
+
+    }
+
+    private List<String> checkDiagonalLeftToRight(String[] dnaMutant) {
+	StringBuilder diagonal = new StringBuilder();
+	List<String> wordList = new ArrayList<>();
+	int dnaLen = dnaMutant.length;
+	for (int x = 0; x <= dnaLen - SECUENCES_DNA_LENGTH; x++) {
+	    diagonal = new StringBuilder();
+	    for (int dr = 0; dr < dnaLen && dr + x < dnaLen; dr++) {
+		if (diagonal.toString().isEmpty() || diagonal.charAt(diagonal.length() - 1) == dnaMutant[dr + x].charAt(dr)) {
+		    diagonal.append(dnaMutant[dr + x].charAt(dr));
+		} else if (diagonal.length() < SECUENCES_DNA_LENGTH) {
+		    diagonal = new StringBuilder();
+		} else {
+		    break;
+		}
+	    }
+	    if (diagonal.length() >= SECUENCES_DNA_LENGTH) {
+		wordList.add(diagonal.toString());
+	    }
+	    if (wordList.size() > 1) {
+		return wordList;
+	    }
+	}
+
+	for (int auxx = 1; auxx <= dnaLen - SECUENCES_DNA_LENGTH; auxx++) {
+	    diagonal = new StringBuilder();
+	    for (int dr = 0; dr + auxx < dnaLen; dr++) {
+		if (diagonal.toString().isEmpty() || diagonal.charAt(diagonal.length() - 1) == dnaMutant[dr].charAt(dr + auxx)) {
+		    diagonal.append(dnaMutant[dr].charAt(dr + auxx));
+		} else if (diagonal.length() < 4) {
+		    diagonal = new StringBuilder();
+		} else {
+		    break;
+		}
+	    }
+	    if (diagonal.length() >= SECUENCES_DNA_LENGTH) {
+		wordList.add(diagonal.toString());
+	    }
+	    if (wordList.size() > 1) {
+		return wordList;
+	    }
+	}
+
+	return wordList;
     }
 
 }
